fb54574eb7657e83872246f9ade79b6fb44229a8
engziara <m.ziara1995@gmail.com>
2016-10-27 04:23:45 +0300

    add Auth And improve the UI

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -1,41 +1,45 @@
 @extends('layout')
 @section('content')
 
-<div class="row">
-<div class="col-md-6 col-md-offset-3">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
 
-<h1>{{ $card->title}}</h1>
+            <h1>{{ $card->title}}</h1>
 
-  <ul class="list-group">
-    @foreach($card->notes as $note)
-      <li class="list-group-item">
-        {{ $note->body }}
-        <a href="#" class="pull-right"> {{ $note->user->username }} </a>
-      </li>
-    @endforeach
-  </ul>
-           <hr>
-<h3>Add New Note</h3>
-  <form method="POST" action="/cards/{{ $card->id }}/notes">
-         {{ csrf_field() }}
-   <input type="text" name="user_id" value="1" hidden>
-          <div class="form-group">
-          <textarea name="body" class="form-control">{{ old('body') }}</textarea>
-          </div>
+            <ul class="list-group">
+                @foreach($card->notes as $note)
+                    <li class="list-group-item">
+                        {{ $note->body }}
+                        @if(Auth::user()->id == $note->user_id)
+                            <a href="/notes/{{ $note->id }}/edit"><span class="glyphicon glyphicon-edit"></span></a>
+                        @endif
+                        <a href="#" class="pull-right"> {{ $note->user->name }} </a>
+                    </li>
+                @endforeach
+            </ul>
+            <hr>
+            <h3>Add New Note</h3>
+            <form method="POST" action="/cards/{{ $card->id }}/notes">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <textarea name="body" class="form-control">{{ old('body') }}</textarea>
+                </div>
 
-           <div class="form-group">
-            <button type="submit" class="btn btn-primary">Add Note</button>
-            </div>
-   </form>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Add Note</button>
+                </div>
+            </form>
 
-    @if(count($errors))
-        <div class="alert alert-danger"><ul>
-           @foreach($errors->all() as $error)
-                   <li> {{ $error }} </li>
-           @endforeach
-        </ul></div>
-    @endif
+            @if(count($errors))
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li> {{ $error }} </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-</div>
-</div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,14 @@ Route::get('/', function () {
 
 Route::get('cards','cardsController@index');
 
-Route::get('cards/{card}','cardsController@show');
+Route::get('cards/{card}','cardsController@show')->name('cards');
 
 Route::post('cards/{card}/notes','notesController@store');
 
 Route::get('notes/{note}/edit','notesController@edit');
 
 Route::patch('notes/{note}','notesController@update');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
